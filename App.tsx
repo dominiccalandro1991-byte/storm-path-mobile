0aac25c00e195781c3fe4c714697e10e4ad61021
BackRoad-Inc <dominic.calandro1991@yahoo.com>
2026-09-02 00:08:37 -0500

    Wire Voltcore telemetry into Storm Path App boot

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { installVoltcoreTelemetry } from './src/telemetry';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StormPathProvider } from './src/state/StormPathStore';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
 export default function App(): React.ReactElement {
+  useEffect(() => {
+    installVoltcoreTelemetry();
+  }, []);
   return (
     <SafeAreaProvider>
       <StormPathProvider>
